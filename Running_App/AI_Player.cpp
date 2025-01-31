5106553eca2b881b10a03975093f5a50bce223dc
George Raafat <georgexraafat@gmail.com>
2025-01-31 04:13:45 +0200

    Add Connect Four game implementation with AI and player classes; include logging for performance metrics

---
+++ Running_App/AI_Player.cpp
@@ -0,0 +1,70 @@
+#include "ConnectFour.hpp"
+
+AI_Player::AI_Player(ConnectFourBoard *board) : Base_AI_Player(board)
+{
+    this->name = "AI Computer Player";
+    std::cout << "My names is " << name << '\n';
+}
+
+int AI_Player::minimax(int alpha, int beta, int depth, int ans[])
+{
+    ++runs;
+    cut = false;
+
+    if (board->is_winner())
+        return board->moves() - 5000;
+    if (board->moves() == 42)
+        return 0;
+    if (depth == 11)
+    {
+        return (board->heuristic());
+    }
+
+    int value;
+    int result = -5000;
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (board->update_board(searchOrder[i]))
+        {
+            std::string state = board->to_string();
+            auto it = myMap.find(state);
+            if (it != myMap.end())
+            {
+                value = it->second;
+            }
+            else
+            {
+                value = -minimax(-beta, -alpha, depth + 1, ans);
+                if (!cut)
+                {
+                    myMap[state] = value;
+                }
+            }
+            board->reset(searchOrder[i]);
+            result = std::max(result, value);
+            alpha = std::max(alpha, result);
+            if (depth == 0 && ans[1] < result)
+            {
+                ans[1] = result;
+                ans[0] = searchOrder[i];
+            }
+            if (beta <= alpha)
+            {
+                cut = true;
+                return (alpha);
+            }
+        }
+    }
+
+    return (result);
+}
+
+int AI_Player::get_move()
+{
+    runs = 0;
+    myMap.clear();
+    int ans[2] = {-1, -5000};
+    minimax(-5000, 5000, 0, ans);
+    return ans[0];
+}
---
+++ Running_App/ConnectFour.hpp
@@ -0,0 +1,117 @@
+#ifndef CONNECT_FOUR_HPP
+#define CONNECT_FOUR_HPP
+
+#include <iostream>
+#include <random>
+#include <unordered_map>
+
+class ConnectFourBoard
+{
+private:
+    int n_rows, n_cols;
+    char board[6][7];
+    int n_moves = 0;
+    int lowest_row[7];
+
+public:
+    ConnectFourBoard();
+
+    bool update_board(int index);
+
+    bool is_winner();
+
+    bool is_draw();
+
+    bool game_is_over();
+
+    int moves();
+
+    void display_board();
+
+    void reset(int index);
+
+    int heuristic();
+
+    std::string to_string();
+};
+
+// Players
+//___________________________________
+
+class Player
+{
+protected:
+    std::string name;
+
+public:
+    virtual int get_move() = 0;
+
+    std::string get_name();
+};
+
+class ConnectFourPlayer : public Player
+{
+private:
+    bool isFirst;
+
+public:
+    ConnectFourPlayer(bool isFirst);
+
+    int get_move();
+};
+
+class RandomPlayer : public Player
+{
+private:
+    std::random_device rd;
+    std::mt19937 generator;
+    std::uniform_int_distribution<int> distribution;
+
+public:
+    // Take a symbol and pass it to parent
+    RandomPlayer();
+    // Generate a random move
+    int get_move();
+};
+
+class Base_AI_Player : public Player
+{
+protected:
+    ConnectFourBoard *board;
+    unsigned long long runs = 0;
+
+public:
+    Base_AI_Player(ConnectFourBoard *board);
+    virtual int get_move() = 0;
+    unsigned long long get_runs() { return runs; }
+};
+
+class AI_Player : public Base_AI_Player
+{
+private:
+    std::unordered_map<std::string, int> myMap;
+    int searchOrder[7] = {3, 4, 2, 5, 1, 6, 0};
+    bool cut;
+
+    int minimax(int alpha, int beta, int depth, int ans[]);
+
+public:
+    // Take a symbol and pass it to parent
+    AI_Player(ConnectFourBoard *);
+    // Generate best move move
+    int get_move();
+};
+
+///////////////////////////////////////////
+class GameManager
+{
+private:
+    ConnectFourBoard *board;
+    Player *players[2];
+
+public:
+    GameManager(ConnectFourBoard *, Player *playerPtr[2]);
+    void run();
+};
+
+#endif
